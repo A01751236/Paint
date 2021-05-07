3e845290137016d7ae3b25929dee3e28dfc4da9f
Javier <a01750400@itesm.mx>
2021-05-06 21:35:09 -0500

    Comentarios

--- Paint.py
+++ Paint.py
@@ -1,15 +1,17 @@
 from turtle import *
 from freegames import vector
 
+
 def line(start, end):
-    "Draw line from start to end."
+    #Dibujar una línea de inicio a fin
     up() #levantar la pluma
-    goto(start.x, start.y) #moverse a algun lugar 
-    down() #regresar la pluma 
+    goto(start.x, start.y) #moverse a algun lugar
+    down() #regresar la pluma
     goto(end.x, end.y)
 
+
 def square(start, end):
-    "Draw square from start to end."
+    #Dibujar un cuadrado de inicio a fin
     up()
     goto(start.x, start.y)
     down()
@@ -21,19 +23,21 @@ def square(start, end):
 
     end_fill()
 
+
 def circle(start, end):
-    "Draw circle from start to end."
+    #Dibujar un círculo de inicio a fin
     up() #levantar la pluma
-    goto(start.x, start.y) #moverse a algun lugar 
-    down() #regresar la pluma 
-    
+    goto(start.x, start.y) #moverse a algun lugar
+    down() #regresar la pluma
+
     import turtle
     turtle.circle((end.x - start.x)/2)
-    
+
     end_fill()
 
+
 def rectangle(start, end):
-    "Draw rectangle from start to end."
+    #Dibujar un rectángulo de inicio a fin
     up()
     goto(start.x, start.y)
     down()
@@ -44,8 +48,9 @@ def rectangle(start, end):
     goto(start.x, start.y)
     end_fill()
 
+
 def triangle(start, end):
-    "Draw triangle from start to end."
+    #Dibujar un triángulo de inicio a fin
     up()
     goto(start.x, start.y)
     down()
@@ -57,21 +62,24 @@ def triangle(start, end):
 
     end_fill()
 
+
 def tap(x, y): #Funcion de clicks
-    "Store starting point or draw shape."
+    #Guardar punto de inicio o dibujar figura.
     start = state['start'] #primer  click none
 
     if start is None:
         state['start'] = vector(x, y) #condicion guardar segunda entrada
-    else: #Se realiza al tener una ubicacion guardada 
+    else: #Se realiza al tener una ubicacion guardada
         shape = state['shape']
         end = vector(x, y)
         shape(start, end)
         state['start'] = None
 
+
 def store(key, value):
-    "Store value in state at key."
+    #Guardar valor en state en key
     state[key] = value
+
 
 state = {'start': None, 'shape': line} #condicion guaradar entrada
 setup(420, 420, 370, 0)
@@ -83,7 +91,8 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
-onkey(lambda: color('yellow'), 'Y')
+onkey(lambda: color('yellow'), 'Y') #color añadido
+#teclas para elegir qué forma se va a hacer
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
